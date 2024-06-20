452e9554c004399f2c2b5a472edc1f6e247e0eb3
swapnil singh <113300936+SWS-swapnilsingh@users.noreply.github.com>
2024-06-20 21:01:56 +0530

    modified file1 and added another file named file2.py

--- file1.py
+++ file1.py
@@ -2,5 +2,14 @@
 for i in range(1, 11):
     print(i)
 
+#print numbers from 11, 20
 for i in range(11, 21):
+    print(i)
+
+# print numbers from 21 to 30
+for i in range(21, 31):
+    print(i)
+
+# print numbers from 31 to 40
+for i in range(31, 41):
     print(i)
